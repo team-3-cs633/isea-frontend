73fb04ad070df2f9f9c7f32c48aba2544d0bc439
rharry <ryandharry@gmail.com>
2022-10-01 12:59:26 -0400

    setup project template

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="header">
+        ISEA: International Student Event Aggregator
+      </div>
+    </div>
+  );
+}
+
+export default App;
