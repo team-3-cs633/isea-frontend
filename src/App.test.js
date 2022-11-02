cdfab03f1fad89436e49718c56c9149ccaa28a11
rharry <ryandharry@gmail.com>
2022-11-02 08:35:37 -0400

    change nav hover back to white, add githib actions

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/ISEA: International Student Event Aggregator/i);
   expect(linkElement).toBeInTheDocument();
 });
